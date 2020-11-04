91605fc96e3ee2802f8dc06123639526f221bd98
Emanuela Ianc <emanuela.ianc@smithhotels.com>
2020-11-04 00:38:31 +0000

    Refs Added the quality limit values to the constants file

--- TypeScript/app/constants.ts
+++ TypeScript/app/constants.ts
@@ -2,3 +2,7 @@ export const AGED_BRIE = 'Aged Brie';
 export const BACKSTAGE_PASSES = 'Backstage passes to a TAFKAL80ETC concert';
 export const SULFURAS = 'Sulfuras, Hand of Ragnaros';
 export const CONJURED = 'Conjured';
+
+export const MIN_QUALITY = 0;
+export const MAX_QUALITY = 50;
+export const MAX_QUALITY_SULFURAS = 80;
