e2290989d90fb0d35ff2097ac669cc008ce02d4f
Emanuela Ianc <emanuela.ianc@smithhotels.com>
2020-11-04 22:05:09 +0000

    Refs Added tests for checking the correct class is used for each product type

--- TypeScript/test/updateItem.spec.ts
+++ TypeScript/test/updateItem.spec.ts
@@ -3,6 +3,9 @@ import { Item } from '../app/gilded-rose';
 import { addMethods } from '../app/updateItem';
 import Product from '../app/products/Product';
 import AgedBrie from '../app/products/AgedBrie';
+import BackstagePasses from '../app/products/BackstagePasses';
+import Conjured from '../app/products/Conjured';
+import Sulfuras from '../app/products/Sulfuras';
 
 
 describe('updateItem', () => {
@@ -23,12 +26,25 @@ describe('updateItem', () => {
             const call = new itemWithMethods(new Item('foo', 10, 20));
             expect(call).to.be.an.instanceof(Product);
         });
-        it('creates an instance of the aged brie product', () => {
+        it('creates an instance of the Aged Brie product', () => {
             const itemWithMethods = addMethods('Aged Brie');
             const call = new itemWithMethods(new Item('Aged Brie', 10, 20));
             expect(call).to.be.an.instanceof(AgedBrie);
         });
+        it('creates an instance of the BackstagePasses product', () => {
+            const itemWithMethods = addMethods('Backstage passes to a TAFKAL80ETC concert');
+            const call = new itemWithMethods(new Item('Backstage passes to a TAFKAL80ETC concert', 10, 20));
+            expect(call).to.be.an.instanceof(BackstagePasses);
+        });
+        it('creates an instance of the Conjured product', () => {
+            const itemWithMethods = addMethods('Conjured Mana Cake');
+            const call = new itemWithMethods(new Item('Conjured Mana Cake', 10, 20));
+            expect(call).to.be.an.instanceof(Conjured);
+        });
+        it('creates an instance of the Sulfuras product', () => {
+            const itemWithMethods = addMethods('Sulfuras, Hand of Ragnaros');
+            const call = new itemWithMethods(new Item('Sulfuras, Hand of Ragnaros', 10, 20));
+            expect(call).to.be.an.instanceof(Sulfuras);
+        });
     });
-
-
 });
